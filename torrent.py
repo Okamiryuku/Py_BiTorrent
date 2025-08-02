17cf00acf4f7a97cea9e9e4c2a1158f63ef427c1
Okamiryuku <okamiryuku@gmail.com>
2025-08-02 12:37:42 -0600

    Decoding the torrent file

---
+++ torrent.py
@@ -0,0 +1,19 @@
+import bencodepy
+import hashlib
+import os
+
+class Torrent:
+    def __init__(self, torrent_file):
+        with open(torrent_file, 'rb') as f:
+            self.meta = bencodepy.decode(f.read())
+        self.announce = self.meta[b'announce'].decode('utf-8')
+        self.info = self.meta[b'info']
+        self.info_hash = hashlib.sha1(bencodepy.encode(self.info)).digest()
+        self.piece_length = self.info[b'piece length']
+        self.pieces = self.info[b'pieces']
+        self.total_length = self._get_total_length()
+        self.files = self._get_files()
+        self.num_pieces = (self.total_length + self.piece_length - 1) // self.piece_length
+        
+    def _get_total_length(self):
+        if b'files'in self.info:
