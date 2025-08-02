1bd607b40483bc3c139992f457a89517349daa8d
Okamiryuku <okamiryuku@gmail.com>
2025-08-02 12:53:54 -0600

    Testing the torrent and making comments

--- torrent.py
+++ torrent.py
@@ -6,20 +6,20 @@ class Torrent:
     def __init__(self, torrent_file):
         with open(torrent_file, 'rb') as f:
             self.meta = bencodepy.decode(f.read())
-        self.announce = self.meta[b'announce'].decode('utf-8')
+        self.announce = self.meta[b'announce'].decode('utf-8') # Tracker URL
         self.info = self.meta[b'info']
-        self.info_hash = hashlib.sha1(bencodepy.encode(self.info)).digest()
-        self.piece_length = self.info[b'piece length']
-        self.pieces = self.info[b'pieces']
+        self.info_hash = hashlib.sha1(bencodepy.encode(self.info)).digest() # SHA-1 hash of info dict
+        self.piece_length = self.info[b'piece length']  # Size of each piece
+        self.pieces = self.info[b'pieces'] # Concatenated SHA-1 hashes of pieces
         self.total_length = self._get_total_length()
         self.files = self._get_files()
         self.num_pieces = (self.total_length + self.piece_length - 1) // self.piece_length
         
     def _get_total_length(self):
-        if b'files'in self.info:
+        if b'files'in self.info: # Multi-file torrent
             return sum(file[b'length'] for file in self.info[b'files'])
         else:
-            return self.info[b'length']
+            return self.info[b'length'] # Single-file torrent
         
     def _get_files(self):
         if b'files' in self.info:
@@ -28,5 +28,5 @@ class Torrent:
             return [(self.info[b'name'].decode(), self.info[b'length'])]
         
 
-torrent = Torrent('cosmos-laundromat.torrent')
-print('URL: ', torrent.announce, '/length: ', torrent.total_length, '/Pieces: ', torrent.num_pieces)
+# torrent = Torrent('cosmos-laundromat.torrent')
+# print('URL: ', torrent.announce, '/length: ', torrent.total_length, '/Pieces: ', torrent.num_pieces)
